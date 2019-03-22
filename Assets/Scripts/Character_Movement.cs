9596af43b476f18f970f773c8528dca9c812bbe5
Кирилл <kosarsMK@gmail.com>
2019-03-22 15:39:05 +0200

    Basic Movement

--- Assets/Scripts/Character_Movement.cs
+++ Assets/Scripts/Character_Movement.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class Character_Movement : MonoBehaviour {
     [Range(0.0f, 1.0f)] [SerializeField] public float MovementSpeed = 0.2f; //Скорость движения
     [Range(0f, 25f)] [SerializeField] public float JumpForce = 5f; //Сила прыжка
-    //private float posX,posY; //Позиция Персонажа
-    private bool isGrouded;  
+    [Range(0f, 1f)] [SerializeField] public float FlyingSpeed = 0.2f;
+    public float VerticalMove;
+
+    private bool isGrouded;
+    [SerializeField] public bool infinitieJump; //Бесконечный прыжок
+    [SerializeField] public bool isFlying; // Полет
+
 
     Rigidbody2D rb2d;
     Animation anim;
-   // Collision2D coll;
+    Collider2D collider;
+
     
 
     // Use this for initialization
     void Start() {
         rb2d = GetComponent<Rigidbody2D>();
-        //coll = GetComponent<Collision2D>();
+        collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -26,26 +32,41 @@ public class Character_Movement : MonoBehaviour {
     }
 
     void FixedUpdate() {
-        MoveX();
-        if (Input.GetKey(KeyCode.Space) && isGrouded)
+        Move();
+
+        if (Input.GetKey(KeyCode.Space) && (isGrouded || infinitieJump))
         {
             rb2d.AddForce(new Vector2(0,JumpForce), ForceMode2D.Impulse);
             isGrouded = false;
         }
     }
-    public void MoveX()
+    public void Move()
     {
-        rb2d.position = new Vector2(rb2d.position.x + MovementSpeed, rb2d.position.y);
+        if (isFlying)
+        {
+            VerticalMove = Input.GetAxis("Vertical");
+            rb2d.position = new Vector2(rb2d.position.x + MovementSpeed, rb2d.position.y + VerticalMove*FlyingSpeed/* * Time.deltaTime*100f*/ );
+        }
+        else 
+        {
+            rb2d.position = new Vector2(rb2d.position.x + MovementSpeed, rb2d.position.y);
+        }
+        
     }
 
-    void OnCollisionEnter2D( Collision2D coll)
+    void OnCollisionEnter2D( Collision2D collision )
     {
-        if (coll.gameObject.tag == "Ground")
+        
+        if (collision.gameObject.tag == "Ground")
         {
-            Debug.Log(coll.gameObject.name);
+            Debug.Log(collision.gameObject.name);
             isGrouded = true;
+        }
+        
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            collider.isTrigger = true;
         }
     }
 }
 
-   // Use OnCollisionEnter2D, and then at command jump, give condition blnCollisionGround:
